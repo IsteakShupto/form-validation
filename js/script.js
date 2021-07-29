91a9940ae573ab107650eb8dc2901ec24fa8a9f2
Isteak Ahammed Shupto <87720051+IsteakShupto@users.noreply.github.com>
2021-07-29 16:30:42 +0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,9 +4,19 @@ const formValidation = (event) => {
   event.preventDefault();
 
   const password = document.querySelector("#password").value;
+  const email = document.querySelector("#email").value;
   const passwordValidation = document.querySelector("#password-validation");
+  const emailValidation = document.querySelector("#email-validation");
 
-  console.log(passwordValidation.classList);
+  console.log(emailValidation);
+
+  if (validateEmail(email)) {
+    emailValidation.classList.remove("block");
+    emailValidation.classList.add("hidden");
+  } else {
+    emailValidation.classList.add("block");
+    emailValidation.classList.remove("hidden");
+  }
 
   if (password.length < 6) {
     passwordValidation.classList.add("block");
@@ -15,6 +25,12 @@ const formValidation = (event) => {
     passwordValidation.classList.remove("block");
     passwordValidation.classList.add("hidden");
   }
+};
+
+const validateEmail = (email) => {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
 };
 
 form.addEventListener("submit", formValidation);
